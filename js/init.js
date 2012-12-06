0674c16ade7b4bcde5efbe9103c6eaf966674cd4
Stephan Rusu <stephan.rusu@gmail.com>
2012-12-06 20:00:40 +0200

    update

--- js/init.js
+++ js/init.js
@@ -31,7 +31,7 @@ function init() {
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 128,128 );
 
-	
+	var ab = 31;
 	
 	//material = new THREE.MeshBasicMaterial( { map: texture });
 	var mesh = new THREE.Mesh( geometry, material );
@@ -42,49 +42,116 @@ function init() {
 	// objects
 	geometry = new THREE.CubeGeometry( 1220, 60, 20 );
 	material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading  });
-	
+	material.color.setHex(0x6949c2);
 	// begin north
 		mesh = new THREE.Mesh( geometry, material );
 		mesh.position.x = 0;
-		mesh.position.y = 31;
+		mesh.position.y = ab;
 		mesh.position.z = -400;
 		scene.add( mesh );
-		material.color.setHex(0x6949c2);
+		
 		objects.push( mesh );
 	// end north
 	// begin south 			
 		mesh = new THREE.Mesh( geometry, material );
 		mesh.position.x = 0;
-		mesh.position.y = 31;
+		mesh.position.y = ab;
 		mesh.position.z = 400;
 		scene.add( mesh );
-		material.color.setHex(0x6949c2);
+		
 		objects.push( mesh );		
 	// end south
 	
 	geometry = new THREE.CubeGeometry( 20, 60, 780 );
 
 	// begin east
-		
 		mesh = new THREE.Mesh( geometry, material );
 		mesh.position.x = 600;
-		mesh.position.y = 31;
+		mesh.position.y = ab;
 		mesh.position.z = 0;
 		scene.add( mesh );
-		material.color.setHex(0x6949c2);
+		
 		objects.push( mesh );	
 	// end east
 	// begin west
 		mesh = new THREE.Mesh( geometry, material );
 		mesh.position.x = -600;
-		mesh.position.y = 31;
+		mesh.position.y = ab;
 		mesh.position.z = 0;
 		scene.add( mesh );
-		material.color.setHex(0x6949c2);
+		
 		objects.push( mesh );
 	//end west
 
-	
+	geometry = new THREE.CubeGeometry(20, 60, 250);
+	//walls
+		
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = 500;
+		mesh1.position.y = ab;
+		mesh1.position.z = 170;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = 250;
+		mesh1.position.y = ab;
+		mesh1.position.z = -190;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = 500;
+		mesh1.position.y = ab;
+		mesh1.position.z = -170;
+		scene.add( mesh1 );
+
+		objects.push( mesh1 );
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = -500;
+		mesh1.position.y = ab;
+		mesh1.position.z = 170;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+	geometry = new THREE.CubeGeometry(250, 60, 20);
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = -365;
+		mesh1.position.y = ab;
+		mesh1.position.z = 55;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = -15;
+		mesh1.position.y = ab;
+		mesh1.position.z = 55;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+		mesh1 = new THREE.Mesh(geometry, material);
+		mesh1.position.x = 385;
+		mesh1.position.y = ab;
+		mesh1.position.z = -305;
+		scene.add( mesh1 );
+		objects.push( mesh1 );
+
+	geometry = new THREE.CubeGeometry(450, 60, 20);
+		mesh2 = new THREE.Mesh(geometry, material);
+		mesh2.position.x = 285;
+		mesh2.position.y = ab;
+		mesh2.position.z = 305;
+		scene.add( mesh2 );
+		objects.push( mesh2 );
+
+		mesh2 = new THREE.Mesh(geometry, material);
+		mesh2.position.x = -285;
+		mesh2.position.y = ab;
+		mesh2.position.z = 305;
+		scene.add( mesh2 );
+		objects.push( mesh2 );
+
+
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	window.addEventListener( 'resize', onWindowResize, false );
