ec4ca14e72a8e02ef320d9c058c87fa886bfd6b1
Stephan Rusu <stephan.rusu@gmail.com>
2012-12-13 09:32:40 +0200

    update init.js

--- js/init.js
+++ js/init.js
@@ -81,89 +81,113 @@ function init() {
 		objects.push( mesh );
 	//end west
 
+	geo_horiz_300 = new THREE.CubeGeometry( 300, 60, 20 );
+	geo_horiz_270 = new THREE.CubeGeometry( 270, 60, 20 );
+	geo_horiz_150 = new THREE.CubeGeometry( 150, 60, 20 );
+	geo_vert_250 = new THREE.CubeGeometry( 20, 60, 250 );
+	geo_vert_150 = new THREE.CubeGeometry( 20, 60, 150 );
+
 	//walls wing east
-	geometry = new THREE.CubeGeometry(20, 60, 250); // verical
-		mesh3 = new THREE.Mesh(geometry, material);	// side right top
+		mesh3 = new THREE.Mesh( geo_vert_250, material );  // side right top
 			mesh3.position.x = 500;
 			mesh3.position.y = ab;
 			mesh3.position.z = 170;
 			scene.add( mesh3 );
 			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // side right bottom 
+		mesh3 = new THREE.Mesh( geo_vert_250, material );  // side right bottom 
 			mesh3.position.x = 500;
 			mesh3.position.y = ab;
 			mesh3.position.z = -170;
 			scene.add( mesh3 );
 			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // side left top 
+		mesh3 = new THREE.Mesh( geo_vert_150, material );  // side left top 
+			mesh3.position.x = 250;
+			mesh3.position.y = ab;
+			mesh3.position.z = -220;
+			scene.add( mesh3 );
+			objects.push( mesh3 );
+		mesh3 = new THREE.Mesh( geo_vert_250, material );  // side left bottom 
 			mesh3.position.x = 250;
 			mesh3.position.y = ab;
 			mesh3.position.z = 170;
 			scene.add( mesh3 );
-			objects.push( mesh3 );
-	geometry = new THREE.CubeGeometry(270, 60, 20); // horizontal
-		mesh3 = new THREE.Mesh(geometry, material); // bottom
+			objects.push( mesh3 );	
+		mesh3 = new THREE.Mesh( geo_horiz_270, material ); // bottom
 			mesh3.position.x = 375;
 			mesh3.position.y = ab;
 			mesh3.position.z = -305;
 			scene.add( mesh3 );
 			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // top
+		mesh3 = new THREE.Mesh( geo_horiz_270, material ); // top
 			mesh3.position.x = 375;
 			mesh3.position.y = ab;
 			mesh3.position.z = 305;
 			scene.add( mesh3 );
 			objects.push( mesh3 );
 
-	//walls wing west
-	geometry = new THREE.CubeGeometry(20, 60, 250);	// vertical		
-		mesh3 = new THREE.Mesh(geometry, material);	// side left top
-			mesh3.position.x = -500;
-			mesh3.position.y = ab;
-			mesh3.position.z = 170;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // side left bottom 
-			mesh3.position.x = -500;
-			mesh3.position.y = ab;
-			mesh3.position.z = -170;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // side right top 
-			mesh3.position.x = -250;
-			mesh3.position.y = ab;
-			mesh3.position.z = 170;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-	geometry = new THREE.CubeGeometry(270, 60, 20); // horizontal
-		mesh3 = new THREE.Mesh(geometry, material); // bottom
-			mesh3.position.x = -375;
-			mesh3.position.y = ab;
-			mesh3.position.z = -305;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // top
-			mesh3.position.x = -375;
-			mesh3.position.y = ab;
-			mesh3.position.z = 305;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
+	//walls wing west	
+		mesh4 = new THREE.Mesh( geo_vert_250, material );  // side left top
+			mesh4.position.x = -500;
+			mesh4.position.y = ab;
+			mesh4.position.z = 170;
+			scene.add( mesh4 );
+			objects.push( mesh4 );
+		mesh4 = new THREE.Mesh( geo_vert_250, material );  // side left bottom 
+			mesh4.position.x = -500;
+			mesh4.position.y = ab;
+			mesh4.position.z = -170;
+			scene.add( mesh4 );
+			objects.push( mesh4 );
+		mesh4 = new THREE.Mesh( geo_vert_150, material );  // side right top
+			mesh4.position.x = -250;
+			mesh4.position.y = ab;
+			mesh4.position.z = -220;
+			scene.add( mesh4 );
+			objects.push( mesh4 );
+		mesh4 = new THREE.Mesh( geo_vert_250, material );  // side right bottom 
+			mesh4.position.x = -250;
+			mesh4.position.y = ab;
+			mesh4.position.z = 170;
+			scene.add( mesh4 );
+			objects.push( mesh4 );		
+		mesh4 = new THREE.Mesh( geo_horiz_270, material ); // bottom
+			mesh4.position.x = -375;
+			mesh4.position.y = ab;
+			mesh4.position.z = -305;
+			scene.add( mesh4 );
+			objects.push( mesh4 );
+		mesh4 = new THREE.Mesh( geo_horiz_270, material ); // top
+			mesh4.position.x = -375;
+			mesh4.position.y = ab;
+			mesh4.position.z = 305;
+			scene.add( mesh4 );
+			objects.push( mesh4 );
 
-	// wall centre nerth
-	geometry = new THREE.CubeGeometry(300, 60, 20); // horizontal
-		mesh3 = new THREE.Mesh(geometry, material); // top
-			mesh3.position.x = 0;
-			mesh3.position.y = ab;
-			mesh3.position.z = -305;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-		mesh3 = new THREE.Mesh(geometry, material); // bottom
-			mesh3.position.x = 0;
-			mesh3.position.y = ab;
-			mesh3.position.z = 55;
-			scene.add( mesh3 );
-			objects.push( mesh3 );
-
+	// wall centre north south	
+		mesh2 = new THREE.Mesh( geo_horiz_300, material ); // top
+			mesh2.position.x = 0;
+			mesh2.position.y = ab;
+			mesh2.position.z = -305;
+			scene.add( mesh2 );
+			objects.push( mesh2 );
+		mesh2 = new THREE.Mesh( geo_horiz_300, material ); // bottom
+			mesh2.position.x = 0;
+			mesh2.position.y = ab;
+			mesh2.position.z = 55;
+			scene.add( mesh2 );
+			objects.push( mesh2 );
+		mesh2 = new THREE.Mesh( geo_vert_150, material );  // right
+			mesh2.position.x = 140;
+			mesh2.position.y = ab;
+			mesh2.position.z = -220;
+			scene.add( mesh2 );
+			objects.push( mesh2 );
+		mesh2 = new THREE.Mesh( geo_vert_150, material );  // left
+			mesh2.position.x = -140;
+			mesh2.position.y = ab;
+			mesh2.position.z = -220;
+			scene.add( mesh2 );
+			objects.push( mesh2 );
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
